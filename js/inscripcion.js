6c1e7f4a21c8047bb2d2f1a580b947b42b4ca990
Ibi <ibieldahuk@gmail.com>
2023-06-19 12:54:11 -0300

    Funcionalidad Form de Inscripción

--- js/inscripcion.js
+++ js/inscripcion.js
@@ -1,63 +1,70 @@
-const agregarPersona =document.getElementById("agregar");
+const agregarPersona = document.getElementById("agregar-persona");
 const botonesSuprimir = document.querySelectorAll("#suprimir");
 let lineaInscripcion = document.querySelector('.linea-inscripcion');
 let montoTotal = document.querySelector("#monto");
 const inscribirse = document.querySelector("#inscribirse");
 let personas = 1;
 let botones = document.querySelectorAll(".opciones");
+let btnsEliminarPersona = document.querySelectorAll(".eliminar-persona");
 
-//inicio agregar persona
-agregarPersona.addEventListener("click", ()=>{
-    var nuevaLineaInscripcion = lineaInscripcion.cloneNode(true);
-    personas++;
-    var inputs = nuevaLineaInscripcion.querySelectorAll('input');
-    inputs.forEach(function(input) {
-        input.value = '';
-    });
-    lineaInscripcion.parentNode.insertBefore(nuevaLineaInscripcion, null);
-    
+//AGREGAR PERSONA
+agregarPersona.addEventListener("click", () => {
+  var nuevaLineaInscripcion = lineaInscripcion.cloneNode(true);
+  personas++;
+  var inputs = nuevaLineaInscripcion.querySelectorAll('input');
+  inputs.forEach(function (input) {
+    input.value = '';
+  });
+  lineaInscripcion.parentNode.insertBefore(nuevaLineaInscripcion, null);
 });
-//fin agregar persona
+
+//ELIMINAR PERSONA
+btnsEliminarPersona.forEach((item) => {
+  item.addEventListener("click", function () {
+    console.log("acá");
+    this.parentNode.remove();
+  });
+});
 
 function toggleClase(boton) {
-    // Recorrer todos los botones y deseleccionar los que están seleccionados
-    botones.forEach(function(btn) {
-      if (btn !== boton && btn.classList.contains('clicked')) {
-        btn.classList.remove('clicked');
-      }
-    });
-  
-    // Alternar la clase del botón actual
-    boton.classList.toggle('clicked');
-    actualizarMonto();
+  // Recorrer todos los botones y deseleccionar los que están seleccionados
+  botones.forEach(function (btn) {
+    if (btn !== boton && btn.classList.contains('clicked')) {
+      btn.classList.remove('clicked');
+    }
+  });
+
+  // Alternar la clase del botón actual
+  boton.classList.toggle('clicked');
+  actualizarMonto();
 };
 
 function actualizarMonto() {
-    var monto = 0;
-    var botonesSeleccionados = document.querySelectorAll('.opciones.clicked');
-    botonesSeleccionados.forEach(function(btn) {
+  var monto = 0;
+  var botonesSeleccionados = document.querySelectorAll('.opciones.clicked');
+  botonesSeleccionados.forEach(function (btn) {
 
-      switch (btn.id) {
-        case 'php':
-          monto += 10000;
-          break;
-        case 'html':
-          monto += 50000;
-          break;
-        case 'css':
-          monto += 25000;
-          break;
-        case 'java':
-          monto += 120000;
-          break;
-        case 'javas':
-          monto += 90000;
-          break;
-        case 'mysql':
-          monto += 60000;
-          break;
-        };
-    });
-    
-    montoTotal.textContent = '$' + monto + '.-';
+    switch (btn.id) {
+      case 'php':
+        monto += 10000;
+        break;
+      case 'html':
+        monto += 50000;
+        break;
+      case 'css':
+        monto += 25000;
+        break;
+      case 'java':
+        monto += 120000;
+        break;
+      case 'javas':
+        monto += 90000;
+        break;
+      case 'mysql':
+        monto += 60000;
+        break;
+    };
+  });
+
+  montoTotal.textContent = '$' + monto + '.-';
 };
---
+++ js/inscripcion2.js
@@ -0,0 +1,47 @@
+let contadorID = 1;
+
+document.querySelector('#agregar_persona').addEventListener("click", function () {
+    contadorID++;
+    //DIV
+    let nodoDiv = document.createElement('div');
+    nodoDiv.setAttribute('class', 'linea_inscripcion');
+    nodoDiv.setAttribute('id', 'linea_inscripcion_' + contadorID);
+    //INPUTS
+    let nodoInNombre = document.createElement('input');
+    nodoInNombre.setAttribute('type', 'text');
+    nodoInNombre.setAttribute('name', 'nombre');
+    nodoInNombre.setAttribute('placeholder', 'Nombre');
+    let nodoInApellido = document.createElement('input');
+    nodoInApellido.setAttribute('type', 'text');
+    nodoInApellido.setAttribute('name', 'apellido');
+    nodoInApellido.setAttribute('placeholder', 'Apellido');
+    let nodoInDNI = document.createElement('input');
+    nodoInDNI.setAttribute('type', 'text');
+    nodoInDNI.setAttribute('name', 'dni');
+    nodoInDNI.setAttribute('placeholder', 'DNI');
+    let nodoInEMail = document.createElement('input');
+    nodoInEMail.setAttribute('type', 'email');
+    nodoInEMail.setAttribute('name', 'email');
+    nodoInEMail.setAttribute('placeholder', 'E-Mail');
+    let nodoInTel = document.createElement('input');
+    nodoInTel.setAttribute('type', 'tel');
+    nodoInTel.setAttribute('name', 'telefono');
+    nodoInTel.setAttribute('placeholder', 'Teléfono');
+    let nodoInBtn = document.createElement('input');
+    nodoInBtn.setAttribute('type', 'button');
+    nodoInBtn.setAttribute('class', 'eliminar_persona');
+    nodoInBtn.setAttribute('value', '-');
+    nodoInBtn.setAttribute('onClick', 'eliminarLinea(' + contadorID + ')');
+    //APPENDS
+    nodoDiv.appendChild(nodoInNombre);
+    nodoDiv.appendChild(nodoInApellido);
+    nodoDiv.appendChild(nodoInDNI);
+    nodoDiv.appendChild(nodoInEMail);
+    nodoDiv.appendChild(nodoInTel);
+    nodoDiv.appendChild(nodoInBtn);
+    document.querySelector('#formulario_inscripcion').appendChild(nodoDiv);
+});
+
+function eliminarLinea(id) {
+    document.querySelector('#linea_inscripcion_' + id).remove();
+}
